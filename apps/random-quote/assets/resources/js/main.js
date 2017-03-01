01349c966d05c51eda4fd8f809562cb1c01d2224
Rex <rex.brooker@gmail.com>
2017-02-28 21:12:02 -0500

    Update main.js

--- apps/random-quote/assets/resources/js/main.js
+++ apps/random-quote/assets/resources/js/main.js
@@ -1,4 +1,3 @@
-// this function will open a twitter post page with fomatted quote text
 function createTwitterPost(jsonData) {
     var quoteText;
     var quotePerson;
@@ -6,20 +5,29 @@ function createTwitterPost(jsonData) {
 
     if (jsonData) {
         quoteText = removeHTMLMarkUp(json[0].content);
-
+        // console.log("quoteText:", quoteText);
         quotePerson = jsonData[0].title;
-
+        // console.log("quotePerson:", quotePerson);
     } else {
         quoteText = $("#quote-text").text();
         quotePerson = $("#quote-person").text();
     }
     var hrefLink = siteLink + '"' + quoteText + '" ' + quotePerson;
-
+    // console.log("hrefLink:", hrefLink);
+    // var linkCharCount = hrefLink.count;
+    // if (linkCharCount > 140) {
+    //   var linkCharCountOver = linkCharCount - 140;
+    //   quoteText = quoteText.substring(0, quoteText.length - linkCharCountOver);
+    //   hrefLink = siteLink + '"' + quoteText + '"' + quotePerson;
+    //   console.log("Quote char reduced");
+    // } else {
+    //   console.log("Quote char unchanged");
+    // }
     return hrefLink;
 }
 
 $(document).ready(function() {
-    // fetch quote when opened
+    // alert("Ready!")
     updateQuote();
 });
 
@@ -31,14 +39,15 @@ $('.fa-twitter').click(function() {
     window.open(createTwitterPost());
 });
 
-function updateQuote() {
+function updateQuote() {  
+    // changeStyleSheet();
     $.ajaxSetup({ cache: false });
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
         var currentQuote = json[0].content;
         currentQuote = removeHTMLMarkUp(currentQuote);
         var title = json[0].title;
-        $("#quote-text").html(currentQuote);
-        $("#quote-person").html("- " + title);
+        $("#quote-text").html(currentQuote); 
+        $("#quote-person").html("- " + title); 
     });
 }
 
@@ -46,3 +55,39 @@ function removeHTMLMarkUp(text) {
     text = text.replace(/<[^>]*>/g, "").trim();
     return text;
 }
+
+// function newColor() {
+//   var colorList =
+//   [ "red", "pink", "purple", "deep_purple",
+//  "indigo", "light_blue", "cyan", "teal",
+//  "green", "light green", "lime", "yellow", 
+//  "amber", "orange", "deep_orange", "grey"];
+
+//   randomNumber = Math.floor(Math.random() * colorList.length);
+//   return colorList[randomNumber];
+// }
+
+// function changeStyleSheet(){
+
+//   var newMaterialStyleSheet = "https://code.getmdl.io/1.3.0/material." + newColor() + "-" + newColor() + ".min.css";
+//   console.log(newMaterialStyleSheet);
+//   $('#material-style-sheet').attr('href',newMaterialStyleSheet);
+// }
+(function() {
+    'use strict';
+    var snackbarContainer = document.querySelector('#demo-snackbar-example');
+    var showSnackbarButton = document.querySelector('#demo-show-snackbar');
+    var handler = function(event) {
+        showSnackbarButton.style.backgroundColor = '';
+    };
+    showSnackbarButton.addEventListener('click', function() {
+        'use strict';
+        var data = {
+            message: 'Developed by Rex Brooker.',
+            timeout: 10000,
+            actionHandler: handler,
+            actionText: 'Okay'
+        };
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+    });
+}());
