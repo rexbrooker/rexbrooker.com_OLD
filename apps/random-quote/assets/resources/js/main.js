d17f9a32512d3d141cc49f616921766c1269449c
rexbrooker <chris.brooker@live.com>
2017-02-28 20:27:40 -0500

    Removed Snack Bar

--- apps/random-quote/assets/resources/js/main.js
+++ apps/random-quote/assets/resources/js/main.js
@@ -46,25 +46,3 @@ function removeHTMLMarkUp(text) {
     text = text.replace(/<[^>]*>/g, "").trim();
     return text;
 }
-window.onload = function() {
-
-
-    (function() {
-        'use strict';
-        var snackbarContainer = document.querySelector('#demo-snackbar-example');
-        var showSnackbarButton = document.querySelector('#demo-show-snackbar');
-        var handler = function(event) {
-            showSnackbarButton.style.backgroundColor = '';
-        };
-        showSnackbarButton.addEventListener('click', function() {
-            'use strict';
-            var data = {
-                message: 'Developed by Rex Brooker.',
-                timeout: 10000,
-                actionHandler: handler,
-                actionText: 'Okay'
-            };
-            snackbarContainer.MaterialSnackbar.showSnackbar(data);
-        });
-    }());
-}
